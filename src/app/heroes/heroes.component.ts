186931179c7102a92582c9928008f940ce0de262
pyBae-py <sajeel.ahmed@protonmail.com>
2020-09-09 21:33:21 +0500

    :under_construction: Complete Section 3

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core'
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes'
+import { MessageService } from '../message.service'
+import { HeroService } from '../hero.service'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES
+  heroes: Hero[]
   selectedHero: Hero
 
-  constructor() {}
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes()
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes))
   }
 }
